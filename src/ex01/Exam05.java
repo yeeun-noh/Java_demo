1317ce04e9a2d2e563c211aa1a573c56a6206c35
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-05 23:57:22 +0900

    Update Exam05.java

--- src/ex01/Exam05.java
+++ src/ex01/Exam05.java
@@ -8,7 +8,7 @@ public class Exam05 {
 		
 		String result = null;
 		
-		// 삼항연산자 or 조건연산자
+		// 삼항연산자 = 조건연산자
 		result = (jumsu >= 60) ? "합격" : "불합격"; // true면 합격 출력, false면 불합격 출력
 		// 조건식 ? 식1(true면 출력) : 식2(false면 출력)
 		System.out.println("당신은 " + result + " 입니다");
