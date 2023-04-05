d8d6bc6849d21bda4a07c2ea687e3ff8caf4cdaa
yeeun-noh <gkdms8517@naver.com>
2023-04-05 15:31:01 +0900

    first commit

---
+++ src/ex01/Exam01.java
@@ -0,0 +1,16 @@
+package ex01;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		
+		boolean power = true; // true or false
+		
+		if(power) // ()안의 내용이 참이면 10번째줄 실행, 거짓이면 12번째줄 실행
+			System.out.println("power는 true");
+		else
+			System.out.println("power는 false");
+		
+	}
+
+}
---
+++ src/ex02/Exam02.java
@@ -0,0 +1,33 @@
+package ex02;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+
+		double d = 85.4;
+		int score = 10;
+		
+		d = score;
+	//  d = (double)score; => 10에서 10.0으로 변환
+		System.out.println(d);
+		
+		d = 85.4;
+		score = (int)d; // 값이 변하기 때문에 자동으로 바뀌지않음
+		System.out.println(score); // 85.4에서 85로 변환
+		
+		int sum = 10;
+		long longsum = 3000000000L; 
+	
+		longsum = sum;
+	//  longsum = (long)sum;
+		System.out.println(longsum);
+		
+		System.out.println(5/2); // 정수 + 정수 이기 때문에 2.5에서 2만 출력
+		System.out.println(5/2.0); // 정수 + 실수 이기 때문에 2.5 출력
+		System.out.println(5/(double)2); // 정수 + 실수 이기 때문에 2.5 출력
+		
+		System.out.println( 5 & 2);
+		System.out.println( 5 | 2);
+	}
+
+}
