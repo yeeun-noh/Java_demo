9ebc7eb93d1c1f47f0089019633c2f157d6cf0de
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-06 16:34:42 +0900

    Update Exam02.java

--- src/ex02/Exam02.java
+++ src/ex02/Exam02.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Exam02 {
 
 	public static void main(String[] args) {
+		//(Test)
 		// 나의 이름, 주소, 나이(만으로입력)를 입력 받아서 출력하는 프로그램 작성..(단 나이는 +1해서 출력)
 		
 		Scanner sn = new Scanner(System.in);
