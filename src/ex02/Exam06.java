260eabc36ff78be762d49c6b812d333eeb6956fd
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-07 00:36:24 +0900

    Update Exam06.java

--- src/ex02/Exam06.java
+++ src/ex02/Exam06.java
@@ -21,7 +21,7 @@ public class Exam06 {
 		System.out.println("세 번째 정수 >>");
 		num3 = sc.nextInt();
 		
-		//최대값 구하기 :
+		// 최대값 구하기 :
 		if(num1>num2) {
 			if(num1>num3) max = num1;
 			else max = num3;
@@ -30,7 +30,7 @@ public class Exam06 {
 			else max= num3;
 		}
 				
-		//최소값 구하기 :
+		// 최소값 구하기 :
 		if(num1>num2) {
 			if(num2>num3) min = num3;
 			else min = num2;
@@ -39,7 +39,7 @@ public class Exam06 {
 			else min = num1;
 		}
 		
-	// 또 다른 방법 :
+		// 또 다른 방법 :
 		max = (num1>num2)?((num1>num3)?num1:num3) : ((num2>num3)?num2:num3);
 		min = (num1>num2)?((num2>num3)?num3:num2) : ((num1>num3)?num3:num1);
 		
