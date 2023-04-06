a64461b80079295e1b0c8f4786da277300a881e1
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-06 15:38:16 +0900

    Create Exam04.java

---
+++ src/ex02/Exam04.java
@@ -0,0 +1,31 @@
+package ex02;
+
+import java.util.Scanner;
+
+public class Exam04 {
+
+	public static void main(String[] args) {
+		// 면접점수 70이상, 실기점수 70이상 합격 아니면 불합격
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("면접점수 입력 >>");
+		int a = sc.nextInt();
+				
+		System.out.println("실기점수 입력 >>");
+		int b = sc.nextInt();
+		
+		if(a>=70) {
+			if(b>=70) {
+				System.out.println("합격입니다!");
+			} else {
+				System.out.println("불합격입니다!");				
+			}
+		}else {
+			System.out.println("불합격입니다!");
+		}
+		
+		System.out.println("프로그램 종료");
+	}
+
+}
