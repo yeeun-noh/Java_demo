be453732d33a5393fee8ca9751ba6eadc15c5845
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-06 14:52:54 +0900

    Update and rename src/ex01/Exam08.java to src/ex02/Exam02.java

--- src/ex02/Exam02.java
+++ src/ex02/Exam02.java
@@ -1,8 +1,8 @@
-package ex01;
+package ex02;
 
 import java.util.Scanner;
 
-public class Exam08 {
+public class Exam02 {
 
 	public static void main(String[] args) {
 		// 나의 이름, 주소, 나이(만으로입력)를 입력 받아서 출력하는 프로그램 작성..(단 나이는 +1해서 출력)
