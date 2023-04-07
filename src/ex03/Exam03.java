93d2e8ae1b8d7cebdb1397345c9bfbbd8b0b191f
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-07 11:13:16 +0900

    Create Exam03.java

---
+++ src/ex03/Exam03.java
@@ -0,0 +1,23 @@
+package ex03;
+
+import java.util.Scanner;
+
+public class Exam03 {
+
+	public static void main(String[] args) {
+		//(Test)
+		// 숫자 하나를 입력 받아서 짝수 인지 홀수 인지 출력
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자 하나 입력>>");
+		int number = sc.nextInt();
+		
+		if(number %2 == 0)
+			System.out.println("짝수");
+		else
+			System.out.println("홀수");
+
+		System.out.println("프로그램 종료");
+	}
+
+}
