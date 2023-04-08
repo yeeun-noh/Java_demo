e316d1ce1875d0bbce232045d3f190e22e3e5c4f
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-09 01:30:12 +0900

    Update Exam09.java

--- src/ex03/Exam09.java
+++ src/ex03/Exam09.java
@@ -6,7 +6,7 @@ public class Exam09 {
 
 	public static void main(String[] args) {
 		//(Test)
-		// 학점 구하기는 switch문으로 변경 (Exam01.java 확인)
+		// 학점 구하기는 switch문으로 변경(Exam01.java 확인)
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("성적 입력>>");
