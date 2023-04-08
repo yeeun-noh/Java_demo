0321ef3aec8f5686ac31a7e7e096a9f1a8f537a8
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-09 01:22:38 +0900

    Update Exam06.java

--- src/ex01/Exam06.java
+++ src/ex01/Exam06.java
@@ -11,7 +11,7 @@ public class Exam06 {
 		System.out.println("정수 입력 >>");
 		int num = sc.nextInt();
 		
-		int result = (num>0) ? num : -num; // -(-50) => 50
+		int result = (num > 0) ? num : -num; // -(-50) => 50
 				
 		System.out.println(result);
 	}
