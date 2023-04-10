ef8ef08a79bd790d74a3782a02b14272d61d9620
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-10 16:57:09 +0900

    Update Exam05.java

--- src/ex04/Exam05.java
+++ src/ex04/Exam05.java
@@ -4,55 +4,55 @@ public class Exam05 {
 
 	public static void main(String[] args) {
 		
+		int sum = 0;
 		int sum2 = 0;
-		int sum3 = 0;
-		int j, k;
+		int i, j;
 		
-		System.out.print("j: ");		
+		System.out.print("i: ");		
+		for(i=1; i<=5; i++) {
+			sum += i;  // 누적(+=) : 1 + 2 + 3 + 4 + 5 = 15
+			System.out.print(i + ", ");
+		}
+		System.out.println("\n(i)총합: " + sum);
+
+		
+		System.out.print("\nj: ");		
 		for(j=1; j<=5; j++) {
-			sum2 += j;  // 1 + 2 + 3 + 4 + 5 = 15
+			sum2 -= j;  // 차감(-=) : (-1) + (-2) + (-3) + (-4) + (-5) = -15
 			System.out.print(j + ", ");
 		}
 		System.out.println("\n(j)총합: " + sum2);
-
-		
-		System.out.print("\nk: ");		
-		for(k=1; k<=5; k++) {
-			sum3 -= k;  // (-1) + (-2) + (-3) + (-4) + (-5) = -15
-			System.out.print(k + ", ");
-		}
-		System.out.println("\n(k)총합: " + sum3);
 
 		System.out.println("---------------------------------------");
 		
 		//(Test)
 		// 1 + (-2) + 3 + (-4) + ..... 이런식으로 더해나갈때, 몇까지 더해야 총합이 100이상 될까?
 		// 이때 i값하고 총합을 출력
-		int sum = 0;
-		int i;
+		int total = 0;
+		int x;
 
-		for(i=0; true; i++) { 	// true 생략 가능
-			if(i % 2 == 1)		// 홀수
-				sum +=i;		// 1 + 3 + 5 + ...
+		for(x=0; true; x++) { 	// true 생략 가능
+			if(x % 2 == 1)		// 홀수
+				total += x;		// 1 + 3 + 5 + ...
 			else				// 짝수
-				sum -=i;  		// (-2) + (-4) + (-6) + ...
+				total -= x;  		// (-2) + (-4) + (-6) + ...
 					
-			if(sum >= 100) break;
+			if(total >= 100) break;
 		}
-		System.out.println("i: " + i + ", 총합: " + sum);
+		System.out.println("x: " + x + ", 총합: " + total);
 		
 		// 또 다른 방법 :
-		int sum_ = 0;
+		int total_ = 0;
 		int sign = 1;
 		int tmp = 0;
 		
-		for(int x=1; true; x++,sign=-sign) { // (sign=-sign)는 2=>-2 로 만들어줌
-			tmp = sign * x;
+		for(int y=1; true; y++,sign=-sign) { // (sign=-sign)는 양수를 정수로 변환
+			tmp = sign * y;
 			
-			sum_ += tmp;
-			if(sum_ >= 100) break;
+			total_ += tmp;
+			if(total_ >= 100) break;
 		}
-		System.out.println("i: " + tmp + ", 총합: " + sum_);
+		System.out.println("x: " + tmp + ", 총합: " + total_);
 	}
 
 }
