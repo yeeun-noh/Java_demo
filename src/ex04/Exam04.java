38bdb3413f0e353bddbd59d513aac12b8d9c701c
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-10 14:25:21 +0900

    Update Exam04.java

--- src/ex04/Exam04.java
+++ src/ex04/Exam04.java
@@ -13,6 +13,19 @@ public class Exam04 {
 			}
 		}
 		System.out.println("3이상 4462 이하에서 짝수인 정수의 합은 " + sum);
+		
+		System.out.println("---------------------------------------");
+		
+		int sum2 = 0;
+		int j=1;
+		
+		for(; ;) { // 초기화, 조건식, 증감식 모두 생략 가능 => 조건식이 생략된 경우, 참으로 간주되어 무한 반복문이 됨
+			if(sum2 >= 10) break; // 무한 반복문 없애기 위해 break문 사용!
+			
+			sum2 += j;
+			System.out.println(sum2);
+		}
+		System.out.println("j의 값: " + j);
 	}
 	
 }
