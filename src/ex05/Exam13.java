813802cad6a2bd00cc50fb7fa4915ca42159b450
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-11 18:04:25 +0900

    Update Exam13.java

--- src/ex05/Exam13.java
+++ src/ex05/Exam13.java
@@ -1,5 +1,7 @@
 package ex05;
 
+import java.util.Scanner;
+
 public class Exam13 {
 
 	public static void main(String[] args) {
@@ -8,11 +10,30 @@ public class Exam13 {
 //		2. 숫자를 받아서 짝수의 값만 합해서 나오는 코드를 작성하시오.
 //		3. 소수점이나 문자를 받으면, 다시 쓰라고 해야하며 일을 끝내야함
 //
-//		정수만 '5'회 입력>
+//		정수만 '5'회 입력
 //		90, 80, 88, 66, 55 ==> 90+80+66 의 합을 구해서 출력
 //
-//		정수만 5회 입력>
-//		90, 66.7(X), 50, 55, aa(X), 80, 45 ==> 다시 쓰라고 해야함
+//		정수만 5회 입력
+//		90, 66.7(X), 50, 55, aa(X), 80, 45
+		
+		Scanner sc = new Scanner(System.in);
+		int count = 0;
+		int i;
+		int sum = 0;
+		
+		System.out.println("몇회 입력>> ");
+		count = sc.nextInt();
+		
+		for(i=0; i<count+1; i++) {
+			System.out.print("숫자를 입력하세요: ");
+			i = sc.nextInt();
+		
+		
+			if (i%2 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println("짝수의 값의 합은 " + sum);
 	}
 
 }
