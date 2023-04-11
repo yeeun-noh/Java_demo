d51f992b9cee0416ab6bbd61bd693434d014ea3f
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-11 12:26:20 +0900

    Update Exam03.java

--- src/ex05/Exam03.java
+++ src/ex05/Exam03.java
@@ -66,6 +66,25 @@ public class Exam03 {
 			}
 			System.out.println();
 		}
+		
+		System.out.println("\n----------------\n");
+		
+		/*
+		 +++++
+		  ++++
+		   +++
+		    ++
+		     +
+		*/
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<5-i; j++) {
+				System.out.print("+");
+			}
+			System.out.println();
+		}		
 	}
 
 }
