ebd951a0fa51bcfcda9166514ca8e55a5f925a3b
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-11 12:26:48 +0900

    Create Exam04.java

---
+++ src/ex05/Exam04.java
@@ -0,0 +1,12 @@
+package ex05;
+
+public class Exam04 {
+
+	public static void main(String[] args) {
+		// 구구단
+		for(int i=2; i<=9; i++)
+			for(int j=1; j<=9; j++)
+				System.out.printf("%d x %d = %2d%n", i, j, i*j);
+	}
+
+}
