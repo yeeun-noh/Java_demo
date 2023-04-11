2a32f65a202bd4cd87f094c09a127d05368f0635
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-11 12:35:05 +0900

    Update Exam04.java

--- src/ex05/Exam04.java
+++ src/ex05/Exam04.java
@@ -4,9 +4,25 @@ public class Exam04 {
 
 	public static void main(String[] args) {
 		// 구구단
+		// (for문)
 		for(int i=2; i<=9; i++)
 			for(int j=1; j<=9; j++)
 				System.out.printf("%d x %d = %2d%n", i, j, i*j);
+
+		System.out.println("\n----------------\n");
+
+		// (while문)
+		int i = 2;
+		int j = 1;
+		
+		while(i<=9) {
+			j = 1;
+			while(j<=9) {
+				System.out.printf("%d x %d = %2d%n", i, j, i*j);
+				j++;
+			}
+			i++;
+		}
 	}
 
 }
