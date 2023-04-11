9cdb1af6bc8cd1fd4e3bc18a6c50406bae0e67eb
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-11 10:13:41 +0900

    Create Exam02.java

---
+++ src/ex05/Exam02.java
@@ -0,0 +1,24 @@
+package ex05;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		/*
+		 +++++
+		 +++++
+		 +++++
+		 행(세로) : i = 3
+		 열(가로) : j = 5
+		*/
+
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<5; j++)
+				System.out.print("+");
+			System.out.println();
+		}
+		// 순서: i가 0일때 j=0,1,2,3,4 => (+++++) => println()
+		// => i가 1일때 j=0,1,2,3,4 => (+++++) => println()
+		// => i가 2일때 j=0,1,2,3,4 => (+++++) => println()
+	}
+
+}
