a2c538b8984e1575945d8625df1ee2e5f82c8c12
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-11 14:57:29 +0900

    Update Exam07.java

--- src/ex05/Exam07.java
+++ src/ex05/Exam07.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exam07 {
 
 	public static void main(String[] args) {
-		// (Test)
+		// (Test) Exam06.java와 비교해보기
 		// int 타입의 변수 num이 있을 때 각 자리의 합을 더한 결과를 출력하는 코드를 완성하라
 		// 만일 변수 num의 값이 12345라면, "1+2+3+4+5" 결과인 15를 출력해야 한다
 		// [주의] 문자열로 변환하지 말고 숫자로만 처리해야한다
