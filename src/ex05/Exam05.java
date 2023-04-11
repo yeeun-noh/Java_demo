b76d9fd184ca8ce3f0ce46b11c4cbd461905a3d5
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-11 12:35:29 +0900

    Update Exam05.java

--- src/ex05/Exam05.java
+++ src/ex05/Exam05.java
@@ -16,21 +16,6 @@ public class Exam05 {
 		
 		System.out.println("\n----------------\n");
 
-		// 구구단
-		int k = 2;
-		int m = 1;
-		
-		while(k<=9) {
-			m = 1;
-			while(m<=9) {
-				System.out.printf("%d x %d = %2d%n", k, m, k*m);
-				m++;
-			}
-			k++;
-		}
-		
-		System.out.println("\n----------------\n");
-
 		int j = 5;
 		while(j-- != 0) {
 			System.out.println(j + " - I can do it");
