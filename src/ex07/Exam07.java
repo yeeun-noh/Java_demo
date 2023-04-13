6116de07e74602f812f03fec18bdddddc046203f
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-13 18:00:54 +0900

    Create Exam07.java

---
+++ src/ex07/Exam07.java
@@ -0,0 +1,29 @@
+package ex07;
+
+public class Exam07 {
+	
+	public static void main(String[] args) {
+		//(Test)
+		/* (실행결과:)
+		1	2	3	4	5
+		16	17	18	19	6
+		15	24	25	20	7	
+		14	23	22	21	8
+		13	12	11	10	9
+		*/
+		
+		int[][] arr = new int[5][5];
+		int count = 0;
+		int row = 0, column = -1;
+	}
+}
+
+/* (실행결과:)
+
+1	2	3	4	5
+16	17	18	19	6
+15	24	25	20	7	
+14	23	22	21	8
+13	12	11	10	9
+
+*/
