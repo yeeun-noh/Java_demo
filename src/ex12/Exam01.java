ba53c3eecc5b0e3aa21b9f506a74a3365e09c5b6
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-20 15:45:18 +0900

    Update Exam01.java

--- src/ex12/Exam01.java
+++ src/ex12/Exam01.java
@@ -53,7 +53,7 @@ public class Exam01 {
 		
 		//상위클래스는 하위클래스 참조가능 (하위클래스는 상위클래스 참조불가능):
 		A aa = new B();			//aa는 A클래스와 Object클래스 접근/사용가능
-		Object ob = new B(); 	//ob는 Object클래스만 접근/사용가능
+		Object ob = new B(); 		//ob는 Object클래스만 접근/사용가능
 
 //		B bb = new A(); 		//불가능 (하위클래스는 상위클래스 참조불가능)
 		B bb = (B)aa;			//가능 (aa의 타입은 'A'이므로 '(B)'를 추가해서 타입을 바꿔주기) ==> bb(B클래스)는 aa처럼 B클래스를 참조하고 있음
@@ -70,13 +70,13 @@ public class Exam01 {
 		ob.tostring();			//B클래스의 tostring()
 
 		//하위클래스가 상위클래스의 함수를 오버라이딩하면, 상위클래스는 하위클래스에 접근가능(단, 오버라이딩한 함수만 접근가능):
-		a.func();				//A클래스의 func(), i의값은 10
-		b.func();				//B클래스의 func(), i의값은 20
-		aa.func();				//B클래스의 func(), i의값은 20
-//		ob.func();				//에러, Object클래스(ob)가 B클래스를 참조하고있지만 (오버라이딩 함수 제외,) Object클래스만 접근/사용가능!
+		a.func();			//A클래스의 func(), i의값은 10
+		b.func();			//B클래스의 func(), i의값은 20
+		aa.func();			//B클래스의 func(), i의값은 20
+//		ob.func();			//에러, Object클래스(ob)가 B클래스를 참조하고있지만 (오버라이딩 함수 제외,) Object클래스만 접근/사용가능!
 
-		b.onlyB();				//B클래스의 onlyB()
-//		aa.onlyB();				//에러, A클래스(aa)가 B클래스를 참조하고있지만 (오버라이딩 함수 제외,) A클래스,Object클래스만 접근/사용가능!				
+		b.onlyB();			//B클래스의 onlyB()
+//		aa.onlyB();			//에러, A클래스(aa)가 B클래스를 참조하고있지만 (오버라이딩 함수 제외,) A클래스,Object클래스만 접근/사용가능!				
 	}
 
 }
