fcc75e2c8e4258acdf7c04b9981302af9a9707d5
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-24 18:01:02 +0900

    Update InterfaceTest.java

--- src/ex14/InterfaceTest.java
+++ src/ex14/InterfaceTest.java
@@ -2,8 +2,9 @@ package ex14;
 //인터페이스(interface)
 
 interface Animals {
-	void eat();
-	public abstract void sleep();
+	void eat();				//public abstract 생략되어 있음
+	public abstract void sleep();		//public abstract 생략가능
+	default void sound() {};		//구현해도되고 구현하지 않아도 되는 메서드(default 추가하기)
 }
 
 class Cats implements Animals {
@@ -14,6 +15,10 @@ class Cats implements Animals {
 	@Override
 	public void sleep() {
 		System.out.println("고양이는 잔다");
+	}
+	
+	public void sound() {
+		System.out.println("야옹");
 	}
 }
 
@@ -36,13 +41,15 @@ public class InterfaceTest {
 //		d.eat();
 //		d.sleep();
 
-		Animals an = new Dogs(); //최상위클래스만 참조함
+		Animals an = new Cats();
+		an.eat();
+		an.sleep();
+		an.sound();
+		
+		an = new Dogs(); //최상위클래스만 참조함
 		an.eat();
 		an.sleep();
 		
-		an = new Cats();
-		an.eat();
-		an.sleep();
 	}
 
 }
