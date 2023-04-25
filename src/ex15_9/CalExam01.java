51d6df601b97fc2329be9cd588b83449f55480fa
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-25 18:02:47 +0900

    Update CalExam01.java

--- src/ex15_9/CalExam01.java
+++ src/ex15_9/CalExam01.java
@@ -4,8 +4,21 @@ public class CalExam01 {
 	
 	public static void main(String[] args) {
 	
-		//람다식으로 바로:
-		Calculable cal = x-> System.out.println("x*3: " + (x*3));
-		cal.calculate(9);
+		//인터페이스 구현:
+		Calculable cal2 = new Test();
+		System.out.println(cal2.calculate(9));
+		
+		//익명으로 구현했을때(익명구현객체):
+		Calculable cal3 = new Calculable() {
+			@Override
+			public int calculate(int x) {
+				return x*x*x;
+			}
+		};
+		System.out.println(cal3.calculate(9));
+
+		//람다식 (함축):
+		Calculable cal = x -> x*x*x; //return도 생략가능
+		System.out.println(cal.calculate(9));
 	}
 }
