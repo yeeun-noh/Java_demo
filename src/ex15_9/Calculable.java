879329dc938fd4b95879f0747326ce3e8a053d02
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-25 18:02:58 +0900

    Update Calculable.java

--- src/ex15_9/Calculable.java
+++ src/ex15_9/Calculable.java
@@ -1,6 +1,15 @@
 package ex15_9;
 
 public interface Calculable {
+//	void calculate(int x);
+	int calculate(int x);
+}
+
+class Test implements Calculable {
+
+	@Override
+	public int calculate(int x) {
+		return x*x*x;
+	}
 	
-	void calculate(int x);
 }
