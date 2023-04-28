dfc60238a76a5a136e8c7d5cfab3042955243822
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-28 12:49:51 +0900

    Create GenericExam01.java

---
+++ src/ex18_4/GenericExam01.java
@@ -0,0 +1,40 @@
+package ex18_4;
+//지네릭스 - 와일드 카드
+//상한 제한
+
+class A {}
+class B extends A {}
+class C extends B {}
+
+class Box<T> {
+	private T ob;
+
+	public T getOb() {
+		return ob;
+	}
+
+	public void setOb(T ob) {
+		this.ob = ob;
+	}
+}
+
+class Unboxer {
+	public static void peekBox(Box<? extends B> box) { //상한 제한
+//	public static void peekBox(Box<? super Integer> box) { //하한 제한
+		System.out.println(box.getOb());
+	}
+}
+
+public class GenericExam01 {
+
+	public static void main(String[] args) {
+
+		Box<A> aBox = new Box<>();
+		Box<B> bBox = new Box<>();
+		Box<C> cBox = new Box<>();
+		
+//		Unboxer.peekBox(aBox); // Box<? extends B> 이므로! B와 B자손들만 가능!
+		Unboxer.peekBox(bBox);
+		Unboxer.peekBox(cBox);
+	}
+}
