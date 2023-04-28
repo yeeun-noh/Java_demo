49a64c5db9ae08c4c47901d976d99f6b1671f1ba
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-04-28 17:39:38 +0900

    Update BoundedWildcardBase.java

--- src/ex18_6/BoundedWildcardBase.java
+++ src/ex18_6/BoundedWildcardBase.java
@@ -1,6 +1,6 @@
 package ex18_6;
 
-class Box<T> {
+class Box<T> { //T == Object == 최상위 클래스
 	private T ob;
 
 	public T getOb() {
@@ -25,6 +25,8 @@ class BoxHandler {
 	public static void outBox(Box<? extends Toy> box) {
 		Toy toy = box.getOb(); 
 //		box.setOb(new Toy()); //=> <? extends Toy> 넣음으로써 set기능 못쓰게 됨
+//		(이유: T는 Object이므로, setOb(T ob)의 T(Object)에 Toy(extends)가 접근/사용 불가 하기 때문 )
+//		(결과적으로 extends를 사용하게 되면 getOb(getter)만 사용가능!)
 		System.out.println(toy);
 	}
 	
@@ -33,6 +35,8 @@ class BoxHandler {
 //	public static void inBox(Box<Toy> box, Toy n) {
 	public static void inBox(Box<? super Toy> box, Toy n) {
 //		Toy toy = box.getOb(); //=> <? super Toy> 넣음으로써 get기능 못쓰게 됨
+//		(이유: T는 Object이므로, setOb(T ob)의 T(Object)에 Toy(super)가 접근/사용 가능하기 때문 )
+//		(결과적으로 super를 사용하게 되면 setOb(setter)만 사용가능!)
 		box.setOb(n); 
 	}
 }
