230a20e8c4a4c469f41c79cefe72541c0f4d0148
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-02 17:02:34 +0900

    Create ExValidCheck.java

---
+++ src/ex19/ex19_1/ExValidCheck.java
@@ -0,0 +1,56 @@
+package ex19_1;
+// 입력한 수식의 괄호가 올바른지를 체크:
+
+import java.util.*;
+
+public class ExValidCheck {
+
+	public static void main(String[] args) {
+
+		if(args.length != 1) {
+			System.out.println("Usage: java ExpValidCheck \"EXPRESSION\"");
+			System.out.println("Example: java ExpValidCheck \"((2+3)*1)+3\"");
+			System.exit(0);
+		}
+		
+		Stack st = new Stack();
+		String expression = args[0];
+		
+		System.out.println("expression: " + expression);
+		
+		try {
+			for(int i=0; i<expression.length(); i++) {
+				char ch = expression.charAt(i);
+				if(ch == '(')
+					st.push(ch + "");
+				else if(ch == ')')
+					st.pop();
+			}
+		
+			if(st.isEmpty())
+				System.out.println("괄호가 일치함");
+			else
+				System.out.println("괄호가 불일치함");
+			
+		} catch(EmptyStackException e) {
+			System.out.println("괄호가 불일치함");
+		}
+	}
+
+}
+
+/* (실행 결과:) Run > Run Configurations > Arguments
+
+//아무것도 넣지 않으면
+Usage: java ExpValidCheck "EXPRESSION"
+Example: java ExpValidCheck "((2+3)*1)+3"
+
+//((2+3)*1)+3 넝으면
+expression: ((2+3)*1)+3
+괄호가 일치함
+
+//(2+3)*1) 넣으면
+expression: (2+3)*1)
+괄호가 불일치함
+
+*/
