b361b99f4e1beda2b19995425f8a8c435a6567b3
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-03 16:49:13 +0900

    Update RamdaEx01.java

--- src/ex20/ex20_3/RamdaEx01.java
+++ src/ex20/ex20_3/RamdaEx01.java
@@ -5,20 +5,20 @@ interface MyFuncton {
 	int max(int a, int b); //추상메서드(abstract생략)
 }
 
-class MyMax implements MyFuncton {
-	@Override
-	public int max(int a, int b) {
-		return a > b ? a: b;
-	}
-}
+//class MyMax implements MyFuncton {
+//	@Override
+//	public int max(int a, int b) {
+//		return a > b ? a: b;
+//	}
+//}
 
 public class RamdaEx01 {
 
 	public static void main(String[] args) {
 
-		MyMax m = new MyMax();
-		int num = m.max(10, 20);
-		System.out.println(num);
+//		MyMax m = new MyMax();
+//		int num = m.max(10, 20);
+//		System.out.println(num);
 
 		MyFuncton m2 = new MyFuncton() { //익명객체
 			@Override //어노테이션
@@ -33,7 +33,6 @@ public class RamdaEx01 {
 
 /* (실행 결과:)
 
-20
 max: 20
 
 */
