bb086b4d0a5f37770853421630721e3007d8d7d3
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-03 14:42:31 +0900

    Update and rename IteratorCollection.java to IteratorEx.java

--- src/ex20/IteratorEx.java
+++ src/ex20/IteratorEx.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class IteratorCollection {
+public class IteratorEx {
 
 	public static void main(String[] args) {
 
