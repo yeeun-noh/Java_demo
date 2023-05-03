6cc5766803ec169eb25ef8a0e76e0ec9934c4f02
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-03 14:17:31 +0900

    Create HashSetCollection.java

---
+++ src/ex20/HashSetCollection.java
@@ -0,0 +1,36 @@
+package ex20;
+//Set - HashSet
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetCollection {
+
+	public static void main(String[] args) {
+
+		Set<String> set = new HashSet<>(); //Set은 중복저장X, 순서X
+		
+		set.add("Toy");
+		set.add("Box");
+		set.add("Robot");
+		set.add("Toy");
+		set.add("Box");
+		set.add("Robot");
+		set.add("kor");
+		set.add("eng");
+		
+		for(String str: set)
+			System.out.println(str);			
+	}
+
+}
+
+/* (실행 결과:)
+
+Box
+Robot
+Toy
+kor
+eng
+
+*/
