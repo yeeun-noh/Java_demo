f6842125c9e84daee19af7d1e64b5beba811ef5f
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-04 15:00:32 +0900

    Create OptionalEx.java

---
+++ src/ex21/ex21_1/OptionalEx.java
@@ -0,0 +1,32 @@
+package ex21_1;
+//Optional<T>
+//Optional<T>객체를 생성할때는 of()나 ofNullable() 사용
+// - get() : Optional객체에 저장된 값을 가져옴
+// - orElse()
+
+import java.util.Optional;
+
+
+public class OptionalEx {
+
+	public static void main(String[] args) {
+
+//		Optional<T> 와 of() 사용:
+		Optional<String> os1 = Optional.of(new String("Toy1"));
+		Optional<String> os2 = Optional.of(new String("Toy2"));
+
+//		get() 사용:
+		if(os1.isPresent())
+			System.out.println(os1.get());
+		else
+			System.out.println(os1.orElse("No..."));
+		
+	}
+
+}
+
+/* (실행 결과:)
+
+Toy1
+
+*/
