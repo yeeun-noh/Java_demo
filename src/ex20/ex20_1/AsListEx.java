0c973c9f5e1c0312094263de997b68614814a90f
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-06 17:01:42 +0900

    Update AsListEx.java

--- src/ex20/ex20_1/AsListEx.java
+++ src/ex20/ex20_1/AsListEx.java
@@ -18,19 +18,23 @@ public class AsListEx {
 		list.add("Robot");
 		list.add("Box");
 		
+		for(String str: list)
+			System.out.println(str);
+		System.out.println("-------------------------");
+
 //		asList사용: 
-		List<String> list2 = Arrays.asList("Toy","Box", "Robot", "Box"); //asList(): 배열을 List에 담아서 반환
+		List<String> list2 = Arrays.asList("Toy2","Box2", "Robot2", "Box2"); //asList(): 배열을 List에 담아서 반환
 		
 		for(String str: list2)
 			System.out.println(str);
-
 		System.out.println("-------------------------");
 		
+//		list2.add("Toy3"); ==> 불가능:
 //		asList()는 반환한 List의 크기를 변경할 수없음 (추가, 삭제 불가능!)
 //		=> 밑의 방법사용:
 //		ArrayList 사용:
 		List<String> list3 = new ArrayList<>(list2);
-		list3.add("Toy2");
+		list3.add("Toy3");
 		
 		for(String str: list3)
 			System.out.println(str);
@@ -39,7 +43,6 @@ public class AsListEx {
 }
 
 /* (실행 결과:)
-
 Toy
 Box
 Robot
@@ -50,5 +53,4 @@ Box
 Robot
 Box
 Toy2
-
 */
