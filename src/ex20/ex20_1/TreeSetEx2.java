f182666c4eb177d46ecf864cd9a7fa006a934318
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-06 20:44:12 +0900

    Update TreeSetEx2.java

--- src/ex20/ex20_1/TreeSetEx2.java
+++ src/ex20/ex20_1/TreeSetEx2.java
@@ -14,9 +14,9 @@ public class TreeSetEx2 {
 		String from = "b";
 		String to = "d";
 		
-		set.add("abc");			set.add("alien");		set.add("bat");	
-		set.add("car");			set.add("Car");			set.add("disc");
-		set.add("dance");		set.add("dZZZZ");		set.add("dzzzz");
+		set.add("abc");		set.add("alien");	set.add("bat");	
+		set.add("car");		set.add("Car");		set.add("disc");
+		set.add("dance");	set.add("dZZZZ");	set.add("dzzzz");
 		set.add("elephant");	set.add("elevator");	set.add("fan");
 		set.add("flower");
 		
