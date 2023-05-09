8a47c66f965eea37ab78df7beea4ffa505891ee4
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-09 14:34:32 +0900

    Update BufferReader.java

--- src/ex23/ex23_1/BufferReader.java
+++ src/ex23/ex23_1/BufferReader.java
@@ -1,6 +1,6 @@
 package ex23_1;
 //Buffer => 두장치 사이의 속도차이를 처리하기 위한 공간 (Buffer에 저장해서 쌓아놓고, 한번에 데이터파일에 저장!, 속도 더 빠름(입출력 효율 높임))
-// - 
+// - BufferedReader() 
 
 import java.io.IOException;
 import java.io.FileReader;
