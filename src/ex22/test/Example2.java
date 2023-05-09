d73ff083d0d251b28b45059b9614079b7d6a0495
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-09 10:36:42 +0900

    Update Example2.java

--- src/ex22/test/Example2.java
+++ src/ex22/test/Example2.java
@@ -10,47 +10,44 @@ import java.util.stream.*;
 import java.util.stream.Collectors.*;
 import java.util.Comparator.*;
 
-class Members {
-	private String name;
-	private String job;
-	
-	public Members(String name, String job) {
-		this.name = name;
-		this.job = job;
-	}
-	
-	public String getName() { return name; }
-	public String getJob() { return job;}
-	
-	@Override
-	public String toString() {
-		return "{name:" + name + ", job:" + job + "}";
-	}
-}
+class Member{
+      private String name;
+      private String job;
+      public Member(String name, String job) {
+         this.name = name;
+         this.job = job;
+      }
+      public String getName() {
+         return name;
+      }
 
-public class Example2 {
+      public String getJob() {
+         return job;
+      }
+      
+      @Override
+      public String toString() {
+         return "{name:" + name + ", job:" + job +"}";
+      }
+   }
 
-	public static void main(String[] args) {
-		
-		List<Members> list = Arrays.asList(
-				new Members("홍길동", "개발자"),
-				new Members("김나라", "디자이너"),
-				new Members("신용권", "개발자")
+   public class Example2 {
+   	public static void main(String[] args) {
+        	List<Member> list = Arrays.asList(
+   		new Member("홍길동","개발자"),
+               	new Member("김나리","디자이너"),
+               	new Member("신용권","개발자")
 		);
-		
-//		Map<String, List<Members>> groupingNo = list.stream()
-//				.collect(groupingBy(Members::getJob,
-//						Collectors.mapping(Members::getName, Collectors.toList()))));
-				
-		System.out.println("[개발자]");
-		
-		groupingMap.get("개발자").stream().forEach(s -> System.out.println(s + " "));
-		System.out.println();
-		
-		System.out.println("[디자이너]");
-		groupingMap.get("디자이너").stream().forEach(s -> System.out.println(s + " "));
-	}
-
+         
+         	Map<String, List<Member>> groupingMap = list.stream()
+                	.collect(Collectors.groupingBy(Member::getJob, Collectors.toList()));
+         			
+         	System.out.println("[개발자]");
+         	groupingMap.get("개발자").forEach(m-> System.out.println(m.toString()));
+         	System.out.println();
+         	System.out.println("[디자이너]");
+         	groupingMap.get("디자이너").forEach(m-> System.out.println(m.toString()));
+        }
 }
 
 /* (실행결과:)
