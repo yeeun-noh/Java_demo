e42f3eb114e0b3176b6c77a12ca5cc007bacb932
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-09 17:54:36 +0900

    Create Example.java

---
+++ src/ex23/ex23_4/Example.java
@@ -0,0 +1,32 @@
+package ex23_4;
+
+public class Example {
+
+	private static Student[] students = {
+			new Student("홍길동", 90, 96),
+			new Student("신용권", 95, 93)
+	};
+	
+	//avg메서드 작성:
+	private static double avg(Function<Student> f) {
+		int sum = 0;
+		for(Student s : students)
+			sum += f.apply(s);
+		return (double)sum/students.length;
+	}
+	
+	public static void main(String[] args) {
+		double englishAvg = avg(s -> s.getEnglishScore() );
+		System.out.println("영어 평균 점수: " + englishAvg);
+		
+		double mathAvg = avg(s -> s.getMathScore() );
+		System.out.println("수학 평균 점수: " + mathAvg);
+	}	
+}
+
+/*
+
+영어 평균 점수: 92.5
+수학 평균 점수: 94.5
+
+*/
