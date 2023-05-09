ffdbaeed5efd01198897b0b0827f7e6da313e163
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-09 18:58:34 +0900

    Update and rename RamdaEx02.java to LambdaEx02.java

--- src/ex20/ex20_3/LambdaEx02.java
+++ src/ex20/ex20_3/LambdaEx02.java
@@ -1,11 +1,11 @@
 package ex20_3;
-//람다식 (Ramda expression)
+//람다식 (Lambda expression)
 
 interface MyFunction2 {
 	int max(int a, int b); //추상메서드(abstract생략)가 하나만 있을때만 람다식 사용가능
 }
 
-public class RamdaEx02 {
+public class LambdaEx02 {
 
 	public static void main(String[] args) {
 
