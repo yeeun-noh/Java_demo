d502f8829b5285a518cf7a74b97787dc844b0202
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-09 16:13:35 +0900

    Create Shape.java

---
+++ src/ex23/ex23_2/Shape.java
@@ -0,0 +1,14 @@
+package ex23_2;
+
+public class Circle implements Shape {
+	 private double radius;
+	 
+	 public Circle(double radius) {
+		 this.radius = radius;
+	 }
+
+	@Override
+	public double getArea() {
+		return Math.PI*radius*radius;
+	}
+}
