8639a403d992f716e1fbc4b910985fc03f8eff05
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-15 19:35:34 +0900

    Update Exercise4_12.java

--- src/Homeworks/src/ex/Exercise4_12.java
+++ src/Homeworks/src/ex/Exercise4_12.java
@@ -3,15 +3,17 @@ package ex;
 
 public class Exercise4_12 {
 
-	public static void main(String[] args) {
-		int result = 0;
-		
-		for(int i=2; i<=9; i++) {
-			for(int j=1; j<=3; j++) {
-				System.out.println(i + "*" + j + "=" + (i*j));
+	public static void main(String[] args) {		
+		for(int i=2; i<9; i=i+3) { 
+			for(int j=1; j<4; j++) {
+				for(int k=i; k<i+3; k++) {
+					System.out.print(k + "*" + j + "=" + (k*j) + "\t");
+					if(k==9) break;
+				}
+				System.out.println();
 			}
+			System.out.println();
 		}
-		
 	}
 
 }
