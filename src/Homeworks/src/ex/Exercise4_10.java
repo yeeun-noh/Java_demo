89f206363a2cde97c5c809ff4de7f5f038662bf1
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-15 19:35:02 +0900

    Update Exercise4_10.java

--- src/Homeworks/src/ex/Exercise4_10.java
+++ src/Homeworks/src/ex/Exercise4_10.java
@@ -10,9 +10,8 @@ public class Exercise4_10 {
 		int sum = 0;
 		
 		while(num != 0) {
-			sum += (num % 10); //10으로 나눈 나머지를 합에 더함 5
-			num -= (num % 10); //1의 결과를 원래의 값에서 뺌 
-			num /= 10;	       //10으로 나눔(자리수 줄이기)
+			sum += (num % 10); 
+			num /= 10;
 		}
 		
 		System.out.println("sum=" + sum); //1+2+3+4+5 => 15
