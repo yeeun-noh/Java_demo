767155160e3da7fb4a29299afc0cf6dd208ceda2
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-23 11:05:16 +0900

    Update Exercise5_11.java

--- src/Tests2/Exercise5_11.java
+++ src/Tests2/Exercise5_11.java
@@ -7,7 +7,7 @@ public class Exercise5_11 {
 	public static void main(String[] args) {
 
 		int[][] score = {
-				{100, 100, 100}
+			    {100, 100, 100}
 			  , {20, 20, 20}
 			  , {30, 30, 30}
 			  , {40, 40, 40}
