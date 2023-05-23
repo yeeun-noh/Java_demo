94160ef0322d675059754c570458c64387cad562
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-05-23 11:16:40 +0900

    Update Exercise5_09.java

--- src/Tests2/Exercise5_09.java
+++ src/Tests2/Exercise5_09.java
@@ -30,6 +30,13 @@ public class Exercise5_09 {
 				result[i2][j2]=star[i][j];
 			}
 		}
+// 		=>위의 식을 밑의 식으로도 가능:
+// 		for(int i=0; i<star.length; i++) {
+// 			for(int j=0; j<star[i].length; j++) {
+// 				result[j][i]=star[star.length-i-1][j];
+// 			}
+// 		}
+		
 		for(int i=0; i<result.length; i++) {
 			for(int j=0; j<result[i].length; j++) {
 				System.out.print(result[i][j]);
